a45ba90ee4a15f05b34efe2f17be11d9e4f5137a
Maryam Khan <maryam.khan@pcc.edu>
2023-03-27 08:58:58 -0700

    Added loading and currentDataset state variables as well as functions for toggling 'loading' variable between true/false and updating currentDataset

--- src/context/StatsContext.jsx
+++ src/context/StatsContext.jsx
@@ -3,12 +3,19 @@ export const StatsContext = createContext();
 
 export const statsContextReducer = (state, action) => {
   switch (action.type) {
+    case "TOGGLE_LOADING":
+      return { ...state, loading: !state.loading };
+    case "UPDATE_DATASET":
+      return { ...state, currentDataset: action.payload };
     default:
       return state;
   }
 };
 export const StatsContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(statsContextReducer, {});
+  const [state, dispatch] = useReducer(statsContextReducer, {
+    loading: true,
+    currentDataset: {},
+  });
   return (
     <StatsContext.Provider value={{ ...state, dispatch }}>
       {children}
