ef89601d0960194b8c4951968cd97545fdd9c05b
Maryam Khan <maryam.khan@pcc.edu>
2023-03-27 08:50:59 -0700

    Added placeholders for links in sidebar and altered styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "../styling/Sidebar.css";
 
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <h1>Equity Pulse</h1>
+      <div className="links">
+        <p>Home</p>
+        <p>About</p>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
