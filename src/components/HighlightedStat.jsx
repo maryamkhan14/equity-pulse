e0145dbc18713577721c0f328fd28be31ce72425
Maryam Khan <maryam.khan@pcc.edu>
2023-03-27 22:00:39 -0700

    Changed HighlightedStat and SummaryStats' styling to emphasize highlighted statistics

--- src/components/HighlightedStat.jsx
+++ src/components/HighlightedStat.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "../styling/HighlightedStat.css";
-const HighlightedStat = ({ title, stat }) => {
+const HighlightedStat = ({ info, title, stat }) => {
   return (
-    <div className="highlighted-stat">
-      <h3>{title}</h3>
-      <p>{stat && stat}</p>
+    <div className={`highlighted-stat ${info}`}>
+      <h3>{stat && stat}</h3>
+      <p>{title}</p>
     </div>
   );
 };
