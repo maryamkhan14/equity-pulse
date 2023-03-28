8673801564ea15fb395bf7cdbde35c94d5bbb87e
Maryam Khan <maryam.khan@pcc.edu>
2023-03-27 23:31:56 -0700

    Altered styling for list of countries' data and removed unnecessary column

--- src/components/StatsList.jsx
+++ src/components/StatsList.jsx
@@ -13,30 +13,33 @@ const StatsList = () => {
           <tr>
             <th>Country</th>
             <th>Median</th>
-            <th>Income/Consumption in Poorest 10%</th>
-            <th>Income/Consumption in Richest 10%</th>
-            <th>Estimated Total</th>
+            <th>Income/Consumption - Poorest 10%</th>
+            <th>Income/Consumption - Richest 10%</th>
           </tr>
         </thead>
         <tbody>
           {Object.keys(fullDataset).length > 0 &&
             fullDataset.map((individualDataset) => {
               let { country_name, country_code } = individualDataset;
-              let { median, decileOneAvg, decileTenAvg, estTotal } =
+              let { median, decileOneAvg, decileTenAvg } =
                 extractRelevantStats(individualDataset);
               return (
                 <tr key={country_code}>
-                  <td>{country_name}</td>
-                  <td>{median}</td>
-                  <td>{decileOneAvg}</td>
-                  <td>{decileTenAvg}</td>
-                  <td>{estTotal}</td>
+                  <td>
+                    <b>{country_name}</b>
+                  </td>
+                  <td>${median}</td>
+                  <td>${decileOneAvg}</td>
+                  <td>${decileTenAvg}</td>
                 </tr>
               );
             })}
-          <tr>
-            {Object.keys(fullDataset).length == 0 && <td>No data yet!</td>}
-          </tr>
+
+          {Object.keys(fullDataset).length == 0 && (
+            <tr>
+              <td>No data yet!</td>
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
