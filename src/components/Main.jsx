e8b8ae8c75a2958f00ea0fd9eca75a86f1c162d9
Maryam Khan <maryam.khan@pcc.edu>
2023-03-27 18:58:19 -0700

    Configured fetching of statistics for USA upon initial load

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,31 @@ import React from "react";
 import "../styling/Main.css";
 import AllStats from "./AllStats";
 import SummaryStats from "./SummaryStats";
+import { useContext, useEffect } from "react";
+import axios from "axios";
+import defaultPovertyQueryParams from "../utilities/defaultPovertyQueryParams";
+import { StatsContext } from "../context/StatsContext";
+
 const Main = () => {
+  const { dispatch } = useContext(StatsContext);
+  const fetchInitial = async () => {
+    let usaStats = await axios({
+      method: "GET",
+      url: `
+    https://api.worldbank.org/pip/v1/pip?country=USA${defaultPovertyQueryParams}`,
+      headers: {
+        Accept: "*/*",
+      },
+    });
+    return usaStats;
+  };
+  useEffect(() => {
+    const initialFetch = async () => {
+      let { data } = await fetchInitial();
+      console.log(...data);
+    };
+    initialFetch();
+  }, []);
   return (
     <div className="main">
       <SummaryStats />
--- src/components/SummaryStats.jsx
+++ src/components/SummaryStats.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "../styling/SummaryStats.css";
-
+import { useEffect, useContext } from "react";
+import { StatsContext } from "../context/StatsContext";
 const SummaryStats = () => {
+  const { currentDataset } = useContext(StatsContext);
+
   return <div className="summary-stats">SummaryStats</div>;
 };
 
