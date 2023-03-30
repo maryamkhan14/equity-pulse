566cdbbfce0d60ad4b1271c64712f0777f7fcdd5
Maryam Khan <maryam.khan@pcc.edu>
2023-03-29 21:41:48 -0700

    Updated data fetching process to pull data from API with interpolation where necessary as well as data without interpolation, then resolve any conflicts between the two datasets in favor of the data that was not interpolated

--- src/utilities/fetchData.js
+++ src/utilities/fetchData.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import defaultPovertyQueryParams from "../constants/defaultPovertyQueryParams";
+
 let fetchCountryStats = async (countryCode) => {
-  let singleCountryStats = await axios({
+  let { data: interpolated } = await axios({
     method: "GET",
     url: `
     https://api.worldbank.org/pip/v1/pip?country=${countryCode}${defaultPovertyQueryParams}`,
@@ -9,7 +10,33 @@ let fetchCountryStats = async (countryCode) => {
       Accept: "*/*",
     },
   });
-  return singleCountryStats;
+  let { data: noninterpolated } = await axios({
+    method: "GET",
+    url: `
+    https://api.worldbank.org/pip/v1/pip?country=${countryCode}&povline=1.9&fill_gaps=false&welfare_type=all&reporting_level=national&format=json&ppp_version=2017&year=2019`,
+    headers: {
+      Accept: "*/*",
+    },
+  });
+  return resolveToInterpolatedData(interpolated, noninterpolated);
 };
 
+let resolveToInterpolatedData = async (interpolated, noninterpolated) => {
+  let noninterpolatedCountries = new Set(
+    noninterpolated.map((item) => item.country_code)
+  );
+
+  return interpolated.map((iCountry) => {
+    if (
+      iCountry.is_interpolated == true &&
+      noninterpolatedCountries.has(iCountry.country_code)
+    ) {
+      return noninterpolated.filter(
+        (nCountry) => nCountry.country_code == iCountry.country_code
+      )[0];
+    } else {
+      return iCountry;
+    }
+  });
+};
 export default fetchCountryStats;
