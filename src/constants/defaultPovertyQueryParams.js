70d449d997aa0ecac7b9b0d2658900c07341e3ad
Maryam Khan <maryam.khan@pcc.edu>
2023-03-29 21:44:28 -0700

    Updated default query parameters for API call

--- src/constants/defaultPovertyQueryParams.js
+++ src/constants/defaultPovertyQueryParams.js
@@ -1,3 +1,3 @@
 let defaultPovertyQueryParams =
-  "&povline=1.9&fill_gaps=true&welfare_type=all&reporting_level=national&format=json&ppp_version=2017&year=2019";
+  "&povline=1.9&fill_gaps=true&welfare_type=all&reporting_level=national&format=json&year=2019";
 export default defaultPovertyQueryParams;
