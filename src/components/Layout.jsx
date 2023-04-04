82258e32d983014d8d982bbb6cac3683a0ea8313
Maryam Khan <maryam.khan@pcc.edu>
2023-04-03 23:26:48 -0700

    Configured styling

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import "../styling/Layout.css";
 const Layout = () => {
   return (
-    <div>
+    <div className="layout">
       <Sidebar />
       <Outlet />
     </div>
